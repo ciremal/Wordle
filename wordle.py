8616b379b4788987ce770b8f69e98c5bfe9057a9
Xavier Chen <xavierchen515@gmail.com>
2022-02-20 19:33:27 -0500

    new commit

--- wordle.py
+++ wordle.py
@@ -1,6 +1,34 @@
+import random
+from colorama import init, Fore
+
+init()
+
+
 def main():
-    pass
+    wordbank = ['clone']
+    tries = 0
+    chosen_word = random.choice(wordbank)
+
+    while tries < 6:
+        print(Fore.WHITE + "", end="")
+        guess = input("Please enter word: ")
+        guessed_word = ["_", "_", "_", "_", "_"]
+        if guess != chosen_word:
+            for letter in range(5):
+                if chosen_word[letter] == guess[letter]:
+                    guessed_word[letter] = chosen_word[letter]
+            for i in range(len(guessed_word)):
+                print(Fore.GREEN + guessed_word[i], end='')
+            print()
+
+            tries += 1
+        else:
+            print("You win!")
+            break
+
+    if tries == 6:
+        print("You lose! The word was", chosen_word)
 
 
 if __name__ == "__main__":
-    pass
+    main()
